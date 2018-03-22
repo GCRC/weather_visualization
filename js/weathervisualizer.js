0645cb04c3b389603e743fcfe3fe604590db56b0
Robert Oikle <6431161+boikle@users.noreply.github.com>
2018-03-22 16:39:23 -0400

    tweaked axis and added graph margin

--- js/weathervisualizer.js
+++ js/weathervisualizer.js
@@ -46,6 +46,9 @@ function weatherVisualization(){
 function lineGraph(data){
     var _this = this;
     this.dataset = data;
+    this.margin = {top: 20, right: 20, bottom: 20, left: 30};
+    this.height = 300 - this.margin.top - this.margin.bottom;
+    this.width = 800 - this.margin.left - this.margin.right;
 
     // Define the xScale
     this.xScale = d3.scale.linear()
@@ -57,21 +60,21 @@ function lineGraph(data){
             var timeHours = (parseInt(d.month) * 30 * 24) + (parseInt(d.day) * 24) + parseInt(d.hour);
             return timeHours;
         })])
-        .range([0, 800]);
+        .range([this.margin.left, this.width]);
 
 
     // Define the yScale
     this.yScale = d3.scale.linear()
-        .domain([d3.min(this.dataset, function(d){
-            if (d.temp_air && parseInt(d.temp_air) !== -9999){
-                return parseInt(d.temp_air);
-            } else {
-                return 0;
-            };
-        }),
-        d3.max(this.dataset, function(d){
-            return d.temp_air})])
-        .range([0,400]);
+        .domain([d3.max(this.dataset, function(d){
+            return d.temp_air}),
+            d3.min(this.dataset, function(d){
+                if (d.temp_air && parseInt(d.temp_air) !== -9999){
+                    return parseInt(d.temp_air);
+                } else {
+                    return 0;
+                };
+            })])
+        .range([this.margin.top, this.height]);
 
     // Define the yAxis
     this.yAxis = d3.svg.axis()
@@ -94,23 +97,29 @@ function lineGraph(data){
                 return _this.xScale(timeHours); 
             })
             .y(function(d) { 
-                var airTemp = (parseInt(d.temp_air) !== -9999)?parseInt(d.temp_air):0;
-                return _this.yScale(airTemp); 
+                //var airTemp = (parseInt(d.temp_air) !== -9999)?parseInt(d.temp_air):0;
+                //return _this.yScale(airTemp); 
+                return _this.yScale(parseInt(d.temp_air)); 
             });
-
+        
         // Add path of line graph
         this.svg.append("path")
             .datum(_this.dataset)
             .attr("class", "line")
+            .attr("transform", "translate(200,0)")
             .attr("d", this.line);
 
-        // Add x/y axis to graph
+        // Add y axis to graph
         this.svg.append("g")
             .attr("class", "axis")
+            .attr("transform", "translate(" + parseInt(_this.margin.left  + 200) +",0)")
             .call(this.yAxis)
+            
+        // Add x axis to graph
+        this.svg.append("g")
+            .attr("class", "axis")
+            .attr("transform", "translate(200," + _this.height + ")")
             .call(this.xAxis);
-
-
     };
 
     // Draw graph
